5f7ad3064ca1531c400c02f048c7faf5be27ce78
Tayyyab khan <iron29700@gmail.com>
2026-01-23 10:58:05 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ from xgboost import XGBRegressor
 # Paths
 # -----------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "data", "train.csv")
+DATA_PATH = os.path.join(BASE_DIR, "data", "bengaluru_house_data.csv")
 MODEL_DIR = os.path.join(BASE_DIR, "model")
 
 os.makedirs(MODEL_DIR, exist_ok=True)
